c559d7c6d961bf79f61a1e73f5819dd3db310af8
Mohit Pandey <50360364+ecomohitpande@users.noreply.github.com>
2021-11-24 12:56:09 +0100

    Create init.R

---
+++ init.R
@@ -0,0 +1,9 @@
+my_packages = c("shiny", "prophet", "lubridate", "dygraphs", "aweek", "plotly")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
